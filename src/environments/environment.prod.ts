29084c2a9527541bc53da254e52d3db06b1bd1cd
Katleho <akhanye@student.wethinkcode.co.za>
2019-01-17 04:02:22 +0200

    changes to the file and fixing of errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5002/api'
-  // apiUrl: 'https://h7a2mdqa3g.execute-api.us-east-1.amazonaws.com/Prod/api'
+  // apiUrl: 'http://localhost:5002/api'
+  apiUrl: 'https://h7a2mdqa3g.execute-api.us-east-1.amazonaws.com/Prod/api'
 };

cea06eaf8cc8c52fcd9ab97cd12918f0fb00bfb5
rmaake <rapula.maake@gmail.com>
2019-01-17 09:19:10 +0200

    Add files via upload

--- src/app/client-feedback/client-feedback/client-feedback.component.ts
+++ src/app/client-feedback/client-feedback/client-feedback.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProjectService } from 'src/app/services/projects/project.service';
+import { Project } from 'src/app/classes/projects/project';
+import { Contact } from 'src/app/classes/client/contact';
+import { ProjectContent } from 'src/app/classes/projects/projectContent';
+import { Timeline } from 'src/app/classes/projects/timeline';
 
 @Component({
   selector: 'app-client-feedback',
@@ -6,10 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-feedback.component.css', '../../client-template/client-template.component.css']
 })
 export class ClientFeedbackComponent implements OnInit {
-
-  constructor() { }
+  projects: Project[] = [];
+  feedbacks: ProjectContent[] = [];
+  selectedFeed: ProjectContent;
+  timelines: Timeline[] = [];
+  constructor(private authenticationService: AuthService, private projService: ProjectService) { }
 
   ngOnInit() {
+    this.projService.getProjectContent().subscribe(res=>{
+      var tmp = this.authenticationService.currentUserValue as Contact;
+      res.forEach(val => {
+        if (val.clientContactInfoId == tmp.clientContactInfoId)
+          this.feedbacks.push(val);
+          
+      });
+      
+      this.projService.getTimelines().subscribe(res=>{
+        this.feedbacks.forEach(val=>{
+          res.forEach(vall=>{
+            if (vall.timelineId == val.timelineId)
+              val.timeline = vall;
+              this.projService.getProject(vall.projectId).subscribe(obj=>{
+                vall.project = obj;
+              })
+          });
+        });
+      });
+    });
+    
+    
   }
-
+  getData(id)
+  {
+    this.feedbacks.forEach(val =>{
+      if (val.projectContentId == id)
+        this.selectedFeed = val;
+    })
+  }
 }
--- src/app/client-feedback/new-feedback/new-feedback.component.ts
+++ src/app/client-feedback/new-feedback/new-feedback.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/classes/projects/project';
+import { Timeline } from 'src/app/classes/projects/timeline';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProjectService } from 'src/app/services/projects/project.service';
+import { Contact } from 'src/app/classes/client/contact';
+import { ProjectContent } from 'src/app/classes/projects/projectContent';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-new-feedback',
@@ -6,10 +13,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-feedback.component.css', '../../client-template/client-template.component.css']
 })
 export class NewFeedbackComponent implements OnInit {
-
-  constructor() { }
+  projects: Project[] = [];
+  timelines: Timeline[] = [];
+  timeofProject: Timeline[] = [];
+  selectedProject = 0;
+  selectedTimline = 0;
+  id = 0;
+  content = new ProjectContent();
+  title = "";
+  constructor(private authenticationService: AuthService, private projService: ProjectService) {
+    
+    this.content.description = "";
+   }
 
   ngOnInit() {
+    var tmp = this.authenticationService.currentUserValue as Contact;
+    this.content.clientContactInfoId = tmp.clientContactInfoId;
+    this.projService.getTimelines().subscribe(res=>{
+      res.forEach(val=>this.timelines.push(val));
+    });
+    this.projService.getProjects().subscribe(res=>{
+      var tmp  = this.authenticationService.currentUserValue as Contact;
+      res.forEach(val=>{
+        if (val.clientId == tmp.clientId)
+          this.projects.push(val);
+        });
+    });
   }
+  projectChange(id: number)
+  {
+   
+    this.timeofProject = [];
+    this.selectedProject = id;
+    this.timelines.forEach(val=>{
+      if (val.projectId == id)
+        this.timeofProject.push(val);
+    });
+  }
+  timelineChange(id: number)
+  {
+    this.content.timelineId = id;
+  }
+  onSubmit()
+  {
+    this.title = (document.getElementById('title') as HTMLInputElement).value;
+    this.content.description = (document.getElementById('msg') as HTMLTextAreaElement).value;
 
+   
+    this.content.description = this.title + "-" + this.content.description;
+  
+    this.projService.newFeedBack(this.content).subscribe(res=>{
+      console.log(res);
+      alert("Success");
+    });
+  }
 }

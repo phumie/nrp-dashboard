6cebeb425e36affe095129475166dbf5340d0dbc
rmaake <rapula.maake@gmail.com>
2019-01-17 09:20:42 +0200

    Add files via upload

--- src/app/client-projects/client-project-view/client-project-view.component.ts
+++ src/app/client-projects/client-project-view/client-project-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProjectService } from 'src/app/services/projects/project.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Timeline } from 'src/app/classes/projects/timeline';
 
 @Component({
   selector: 'app-client-project-view',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-project-view.component.css', '../../client-template/client-template.component.css']
 })
 export class ClientProjectViewComponent implements OnInit {
+id = 0;
+timelines: Timeline[] = [];
+  constructor(private authenticationService: AuthService, private projService: ProjectService, private route: ActivatedRoute) {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
 
-  constructor() { }
+   }
 
   ngOnInit() {
+    this.projService.getTimelines().subscribe(res =>{
+      console.log(res);
+      res.forEach(val=>{
+        if(val.projectId == this.id)
+          this.timelines.push(val);
+      })
+    });
   }
 
 }
--- src/app/client-projects/client-projects/client-projects.component.ts
+++ src/app/client-projects/client-projects/client-projects.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/classes/projects/project';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProjectService } from 'src/app/services/projects/project.service';
+import { Contact } from 'src/app/classes/client/contact';
 
 @Component({
   selector: 'app-client-projects',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientProjectsComponent implements OnInit {
 
-  constructor() { }
+  projects: Project[] = [];
+  constructor(private authenticationService: AuthService, private projService: ProjectService) { }
 
   ngOnInit() {
+    this.projService.getProjects().subscribe(res =>{
+      res.forEach(val =>{
+        var tmp = this.authenticationService.currentUserValue as Contact;
+        if (val.clientId == tmp.clientId)
+          this.projects.push(val);
+      });
+    }
+
+    );
   }
 
 }

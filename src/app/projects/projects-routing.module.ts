335dcc79230e76e3e827605ca79107775a4120a6
Phuti MATLE <pmatle@e4r19p2.wethinkcode.co.za>
2019-01-25 16:00:04 +0200

    switched view-timeline with :id url

--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        // canActivateChild: [AuthGuard],
+        canActivateChild: [AuthGuard],
         children: [
           {
             path: 'new-project',
